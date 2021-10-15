4b7ad72bc832e198c99f548458a0103a10a04a69
phxswjjj <phxswjjj@gmail.com>
2021-10-15 22:47:53 +0800

    init

---
+++ DeployWinFormClickOnce/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeployWinFormClickOnce
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var env = System.Configuration.ConfigurationManager.AppSettings["ENV"];
+            this.Text = $"ENV={env}";
+        }
+    }
+}
